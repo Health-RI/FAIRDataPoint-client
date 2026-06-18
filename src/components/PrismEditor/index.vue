eb2e379ef08e8a80f693d9e7401eaa1fd1c01be1
Hans-christian <hanschristian.vanderwerf@health-ri.nl>
2026-06-18 11:36:26 +0200

    Revert Prism workaround and pin Vite 8.0.14

--- src/components/PrismEditor/index.vue
+++ src/components/PrismEditor/index.vue
@@ -9,18 +9,6 @@
 import { defineComponent } from 'vue'
 import { PrismEditor as BasePrismEditor } from 'vue-prism-editor'
 import Prism from 'prismjs'
-import { registerPrismLanguages } from '@/prism-languages'
-
-registerPrismLanguages(Prism)
-
-function escapeHtml(code: string): string {
-  return code
-    .replace(/&/g, '&amp;')
-    .replace(/</g, '&lt;')
-    .replace(/>/g, '&gt;')
-    .replace(/"/g, '&quot;')
-    .replace(/'/g, '&#39;')
-}
 
 export default defineComponent({
   name: 'PrismEditor',
@@ -38,18 +26,8 @@ export default defineComponent({
     const highlight = (code: string) => {
       const lang = props.language || 'markup'
       const languages = (Prism as any).languages || {}
-      const grammar = languages[lang]
-
-      if (!grammar) {
-        return escapeHtml(code)
-      }
-
-      try {
-        return Prism.highlight(code, grammar, lang)
-      } catch (error) {
-        console.warn(`Prism highlighting failed for "${lang}", falling back to plain text.`, error)
-        return escapeHtml(code)
-      }
+      const grammar = languages[lang] || languages.markup
+      return Prism.highlight(code, grammar, lang)
     }
 
     return {
